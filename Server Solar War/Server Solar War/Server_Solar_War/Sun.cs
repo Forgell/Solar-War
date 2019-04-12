68c2a2d029466fee855983077607987327c9e4fc
228634 <228634@S020RMD114-05>
2019-04-12 12:12:34 -0500

    Sun class

---
+++ Server Solar War/Server Solar War/Server_Solar_War/Sun.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.IO;
+
+
+namespace Server_Solar_War
+{
+    class Sun
+    {
+        private Texture2D display;
+        private Texture2D[] img;
+        private Rectangle rect;
+        private int radiusH;
+        
+
+        public Sun()
+        {
+            rect = new Rectangle(200, 0, 100, 100);
+            radiusH = 20;
+            //change the radius 
+        }
+    }
+}
